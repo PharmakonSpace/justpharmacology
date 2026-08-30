c79e3f5ebce4b05a6063793fbabcb06fb16e5439
PharmakonSpace <pharmakonspace@gmail.com>
2026-08-30 15:16:00 +0530

    Update index.js

--- src/data/pharmacology/Healthcare_Psychology/index.js
+++ src/data/pharmacology/Healthcare_Psychology/index.js
@@ -1,29 +1,6 @@
-import { lessons as dosage_regimensLessons } from './dosage-regimens.js';
-import { lessons as dose_responseLessons } from './dose-response.js';
-import { lessons as drug_administrationLessons } from './drug-administration.js';
-import { lessons as drug_interactionsLessons } from './drug-interactions.js';
-import { lessons as drug_responseLessons } from './drug-response.js';
-import { lessons as drug_safetyLessons } from './drug-safety.js';
-import { lessons as foundationsLessons } from './foundations.js';
-import { lessons as individual_variationLessons } from './individual-variation.js';
-import { lessons as pharmacodynamicsLessons } from './pharmacodynamics.js';
-import { lessons as pharmacokineticsLessons } from './pharmacokinetics.js';
-import { lessons as rapid_revisionLessons } from './rapid-revision.js';
-import { lessons as receptorsLessons } from './receptors.js';
-import { lessons as advancedConceptsLessons } from './advanced-concepts.js';
+import { lessons as dosage_regimensLessons } from './Healthcare_Psychology_and_com.js';
 
-export { lessons as dosage_regimensLessons } from './dosage-regimens.js';
-export { lessons as dose_responseLessons } from './dose-response.js';
-export { lessons as drug_administrationLessons } from './drug-administration.js';
-export { lessons as drug_interactionsLessons } from './drug-interactions.js';
-export { lessons as drug_responseLessons } from './drug-response.js';
-export { lessons as drug_safetyLessons } from './drug-safety.js';
-export { lessons as foundationsLessons } from './foundations.js';
-export { lessons as individual_variationLessons } from './individual-variation.js';
-export { lessons as pharmacodynamicsLessons } from './pharmacodynamics.js';
-export { lessons as pharmacokineticsLessons } from './pharmacokinetics.js';
-export { lessons as rapid_revisionLessons } from './rapid-revision.js';
-export { lessons as receptorsLessons } from './receptors.js';
-export { lessons as advancedConceptsLessons } from './advanced-concepts.js';
+export { lessons as dosage_regimensLessons } from './Healthcare_Psychology_and_com.js';
 
-export const generalPharmacologyLessons = [dosage_regimensLessons,dose_responseLessons,drug_administrationLessons,drug_interactionsLessons,drug_responseLessons,drug_safetyLessons,foundationsLessons,individual_variationLessons,pharmacodynamicsLessons,pharmacokineticsLessons,rapid_revisionLessons,receptorsLessons,advancedConceptsLessons].flat();
+
+export const generalPharmacologyLessons = [Healthcare_Psychology_and_communication].flat();
