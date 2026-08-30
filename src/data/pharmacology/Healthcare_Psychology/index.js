cc4f937ffdd7913624bda8a9564d3560ead82e7b
PharmakonSpace <pharmakonspace@gmail.com>
2026-08-30 15:16:37 +0530

    Update index.js

--- src/data/pharmacology/Healthcare_Psychology/index.js
+++ src/data/pharmacology/Healthcare_Psychology/index.js
@@ -1,6 +1,6 @@
-import { lessons as dosage_regimensLessons } from './Healthcare_Psychology_and_com.js';
+import { lessons as dosage_regimensLessons } from './Healthcare_Psychology_and_communication_skill.js';
 
-export { lessons as dosage_regimensLessons } from './Healthcare_Psychology_and_com.js';
+export { lessons as dosage_regimensLessons } from './Healthcare_Psychology_and_communication_skill.js';
 
 
-export const generalPharmacologyLessons = [Healthcare_Psychology_and_communication].flat();
+export const generalPharmacologyLessons = [Healthcare_Psychology_and_communication_skill].flat();
