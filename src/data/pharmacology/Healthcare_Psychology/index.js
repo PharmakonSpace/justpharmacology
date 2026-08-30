437fdc95ff75fbb4d12df471f4c9bbfd06b2b154
PharmakonSpace <pharmakonspace@gmail.com>
2026-08-30 18:36:47 +0530

    Update index.js

--- src/data/pharmacology/Healthcare_Psychology/index.js
+++ src/data/pharmacology/Healthcare_Psychology/index.js
@@ -1,7 +1,9 @@
 import { lessons as TheSPIKESProtocolLessons } from './Healthcare_Psychology_and_communication_skill.js';
+import { lessons as bodyLanguageThatHealsLesson } from './BodyLanguageThatHeals.js';
 
 export { lessons as TheSPIKESProtocolLessons } from './Healthcare_Psychology_and_communication_skill.js';
+export { lessons as bodyLanguageThatHealsLesson } from './BodyLanguageThatHeals.js';
 
 
-export const HealthcarePsychologyandcommunicationskillLessons = [TheSPIKESProtocolLessons].flat();
+export const HealthcarePsychologyandcommunicationskillLessons = [TheSPIKESProtocolLessons,bodyLanguageThatHealsLesson].flat();
 
