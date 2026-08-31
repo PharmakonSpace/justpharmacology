221bcd9db08b3d49a16e9feab69be055dc67330e
PharmakonSpace <pharmakonspace@gmail.com>
2026-08-31 12:20:01 +0530

    Update Animation.jsx

--- src/components/animations/Animation.jsx
+++ src/components/animations/Animation.jsx
@@ -4,6 +4,8 @@ import AgonistAntagonist from './AgonistAntagonist';
 import AbsorptionAnimation from './AbsorptionAnimation';
 import Spikes from './spikes';
 import Soler from './soler';
+import Soler from './biopsychosocial';
+
 
 export default function Animation({ type }) {
   const normalizedType = type ? type.toLowerCase().trim() : '';
@@ -13,6 +15,7 @@ export default function Animation({ type }) {
   if (normalizedType === 'agonist-antagonist') return <AgonistAntagonist />;
   if (normalizedType === 'spikes') return <Spikes />;
   if (normalizedType === 'soler') return <Soler />;
+  if (normalizedType === 'biopsychosocial') return <Biopsychosocial />;
 
   return <DrugReceptor />;
 }
